f131b997b877d69c410f4529ea9bd95a272403b4
jdbaudean <jdbaudean@gmail.com>
2013-11-20 22:54:25 -0500

    Got the check toggle box mostly working.  The only problem is that the check boxes aren't cleared when items are deleted.

--- RunLogProject/RunLog/src/main/java/umuc/cmsc495/runlog/FragmentHistory.java
+++ RunLogProject/RunLog/src/main/java/umuc/cmsc495/runlog/FragmentHistory.java
@@ -16,15 +16,19 @@ import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class FragmentHistory extends ListFragment {
     private RunsDataSource dataSource;
     //SimpleCursorAdapter adapter;
     ArrayAdapter<RunLogTable> adapter;
     ListView listView;
-    long listId;
-    long listid;
+    Set<Integer> checkedIds;
+    List<RunLogTable> runLogTables;
+    List<CheckedTextView> checkedTextViews;
 
     public static Fragment newInstance(Context context) {
         FragmentHistory f = new FragmentHistory();
@@ -45,7 +49,9 @@ public class FragmentHistory extends ListFragment {
 
         //ArrayAdapter<RunLogTable> adapter = new ArrayAdapter<RunLogTable>(getActivity(), android.R.layout.simple_list_item_checked, values);
         adapter = new ArrayAdapter<RunLogTable>(getActivity(), android.R.layout.simple_list_item_checked, values);
-
+        checkedIds = new HashSet<Integer>();
+        runLogTables = new ArrayList<RunLogTable>();
+        checkedTextViews = new ArrayList<CheckedTextView>();
 
         listView = new ListView(getActivity());
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
@@ -66,11 +72,21 @@ public class FragmentHistory extends ListFragment {
             public void onClick(View view) {
                 //long checkId = adapter.getItemId(0);
 
-                dataSource.deleteRun(listid);
+                for (Integer ids : checkedIds) {
+                    //dataSource.deleteRun(adapter.getItem(ids).getId());
+                }
+
+                for (RunLogTable runLogTable : runLogTables) {
+                    adapter.remove(runLogTable);
+                }
+
+
+                checkedIds.clear();
+                //dataSource.deleteRun(listid);
                 //listView.clearChoices();
-                Log.d("My Tag", "Delete button pressed " + listid);
+
                 //adapter.changeCursor(dataSource.getAllRuns());
-                adapter.remove(adapter.getItem((int) listId));
+                //adapter.remove(adapter.getItem((int) listId));
                 adapter.notifyDataSetChanged();
 
             }
@@ -91,17 +107,27 @@ public class FragmentHistory extends ListFragment {
     public void onListItemClick(ListView l, View view, int position, long id) {
         super.onListItemClick(l, view, position, id);
         CheckedTextView textView = (CheckedTextView) view;
-        textView.setChecked(!textView.isChecked());
+        textView.getId();
+
         //listView.setItemChecked(position, true);
 
-        listId = position;
-        listid = adapter.getItem(position).getId();
-        //adapter.get
-        //Log.d("My Tag", "OnListItemClick " + position);
-        //Log.d("My Tag", "getSelectedItemID" + getSelectedItemId() + "Position " + getSelectedItemPosition());
-        Log.d("My Tag", "id; " + id + "position: " + position);
-        Log.d("My Tag", " " + l.getCheckedItemIds() +
-                " " + adapter.getItem(position).getId());
+
+        Log.d("My Tag", "adapter position: " + adapter.getItem(position).getId());
+        //Log.d("My Tag", "adapter position: " + checkedIds);
+        //listid = adapter.getItem(position).getId();
+        if (!textView.isChecked()) {
+            //checkedIds.add(adapter.getItem(position).getId());
+            checkedIds.add(position);
+            runLogTables.add(adapter.getItem(position));
+        }
+
+        if (textView.isChecked()) {
+            //checkedIds.remove(adapter.getItem(position).getId());
+            checkedIds.remove(position);
+            runLogTables.remove(adapter.getItem(position));
+        }
+        textView.setChecked(!textView.isChecked());
+        //textView.toggle();
 
         //Log.d("My Tag", "adapter.getItem " + adapter.getItemId(id));
 
